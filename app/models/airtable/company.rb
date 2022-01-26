4332e2877dddc9b98f033411a1e01411e44e336e
Leonid Medovyy <leonid@storypro.io>
2022-01-25 18:47:29 -0700

    setup airtable

--- app/models/airtable/company.rb
+++ app/models/airtable/company.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
-class Acompany < Airrecord::Table
-  self.base_key = "appJsinKEWPlytT8z"
-  self.table_name = "Companies"
+module Airtable
+  class Company < Airrecord::Table
+    self.base_key = "appJsinKEWPlytT8z"
+    self.table_name = "Companies"
+  end
 end
