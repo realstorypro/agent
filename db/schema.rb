06bf814cfe00a660ddfbc741ea4d9a4cbc0a3197
Leonid Medovyy <leonid@storypro.io>
2022-02-05 06:46:34 -0700

    added ability for agent to grab companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_125906) do
+ActiveRecord::Schema.define(version: 2022_02_05_132052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_02_04_125906) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "found", default: false
     t.boolean "error", default: false
+    t.string "agent"
     t.index ["name"], name: "index_companies_on_name", unique: true
   end
 
